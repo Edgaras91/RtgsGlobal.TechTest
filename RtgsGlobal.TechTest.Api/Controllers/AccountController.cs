5bcd168bebebcaf7e4a5051ed08c1650caf88e8e
Edgaras <edgaras.valius@h2ecommerce.com>
2023-01-26 18:36:16 +0000

    Changed amount data types to decimal, refactored AddTransaction

--- RtgsGlobal.TechTest.Api/Controllers/AccountController.cs
+++ RtgsGlobal.TechTest.Api/Controllers/AccountController.cs
@@ -6,48 +6,48 @@ namespace RtgsGlobal.TechTest.Api.Controllers;
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
-	private readonly IAccountProvider _accountProvider;
+    private readonly IAccountProvider _accountProvider;
 
-	public AccountController(IAccountProvider accountProvider)
-	{
-		_accountProvider = accountProvider;
-	}
+    public AccountController(IAccountProvider accountProvider)
+    {
+        _accountProvider = accountProvider;
+    }
 
-	[HttpPost("{accountIdentifier}", Name = "Deposit")]
-	public IActionResult Deposit(string accountIdentifier, [FromBody] float amount)
-	{
-		_accountProvider.Deposit(accountIdentifier, amount);
-		return Ok();
-	}
+    [HttpPost("{accountIdentifier}", Name = "Deposit")]
+    public IActionResult Deposit(string accountIdentifier, [FromBody] decimal amount)
+    {
+        _accountProvider.Deposit(accountIdentifier, amount);
+        return Ok();
+    }
 
-	[HttpPost("{accountIdentifier}/withdraw", Name = "Withdrawal")]
-	public IActionResult Withdraw(string accountIdentifier, [FromBody] float amount)
-	{
-		_accountProvider.Withdraw(accountIdentifier, amount);
-		return Ok();
-	}
+    [HttpPost("{accountIdentifier}/withdraw", Name = "Withdrawal")]
+    public IActionResult Withdraw(string accountIdentifier, [FromBody] decimal amount)
+    {
+        _accountProvider.Withdraw(accountIdentifier, amount);
+        return Ok();
+    }
 
-	[HttpPost("transfer", Name = "Transfer")]
-	public IActionResult Transfer(MyTransferDto transfer)
-	{
-		_accountProvider.Transfer(transfer);
-		return Accepted();
-	}
+    [HttpPost("transfer", Name = "Transfer")]
+    public IActionResult Transfer(MyTransferDto transfer)
+    {
+        _accountProvider.Transfer(transfer);
+        return Accepted();
+    }
 
-	[HttpGet("{accountIdentifier}", Name = "GetBalance")]
-	public MyBalance Get(string accountIdentifier) => _accountProvider.GetBalance(accountIdentifier);
+    [HttpGet("{accountIdentifier}", Name = "GetBalance")]
+    public MyBalance Get(string accountIdentifier) => _accountProvider.GetBalance(accountIdentifier);
 }
 
 public class MyTransferDto
 {
-	public MyTransferDto(string debtorAccountIdentifier, string creditorAccountIdentifier, float amount)
-	{
-		DebtorAccountIdentifier = debtorAccountIdentifier;
-		CreditorAccountIdentifier = creditorAccountIdentifier;
-		Amount = amount;
-	}
+    public MyTransferDto(string debtorAccountIdentifier, string creditorAccountIdentifier, decimal amount)
+    {
+        DebtorAccountIdentifier = debtorAccountIdentifier;
+        CreditorAccountIdentifier = creditorAccountIdentifier;
+        Amount = amount;
+    }
 
-	public string DebtorAccountIdentifier { get; set; }
-	public string CreditorAccountIdentifier { get; set; }
-	public float Amount { get; set; }
+    public string DebtorAccountIdentifier { get; set; }
+    public string CreditorAccountIdentifier { get; set; }
+    public decimal Amount { get; set; }
 }
--- RtgsGlobal.TechTest.Api/IAccountProvider.cs
+++ RtgsGlobal.TechTest.Api/IAccountProvider.cs
@@ -4,39 +4,41 @@ namespace RtgsGlobal.TechTest.Api;
 
 public interface IAccountProvider
 {
-	MyBalance GetBalance(string accountIdentifier);
-	void Deposit(string accountIdentifier, float amount);
-	void Transfer(MyTransferDto transfer);
-	void Withdraw(string accountIdentifier, float amount);
+    MyBalance GetBalance(string accountIdentifier);
+    void Deposit(string accountIdentifier, decimal amount);
+    void Transfer(MyTransferDto transfer);
+    void Withdraw(string accountIdentifier, decimal amount);
 }
 
 public class AccountProvider : IAccountProvider
 {
-	private readonly IDictionary<string, MyBalance> _accounts;
+    private readonly IDictionary<string, MyBalance> _accounts;
 
-	public AccountProvider()
-	{
-		_accounts = new Dictionary<string, MyBalance> { { "account-a", new MyBalance() }, { "account-b", new MyBalance() } };
-	}
+    public AccountProvider()
+    {
+        _accounts = new Dictionary<string, MyBalance> {{"account-a", new MyBalance()}, {"account-b", new MyBalance()}};
+    }
 
-	public MyBalance GetBalance(string accountIdentifier) => _accounts[accountIdentifier];
+    public MyBalance GetBalance(string accountIdentifier) => _accounts[accountIdentifier];
 
-	public void Deposit(string accountIdentifier, float amount) => AddTransaction(accountIdentifier, amount);
+    public void Deposit(string accountIdentifier, decimal amount) => AddTransaction(accountIdentifier, amount);
 
-	public void Transfer(MyTransferDto transfer)
-	{
-		AddTransaction(transfer.DebtorAccountIdentifier, -transfer.Amount);
-		AddTransaction(transfer.CreditorAccountIdentifier, transfer.Amount);
-	}
+    public void Transfer(MyTransferDto transfer)
+    {
+        AddTransaction(transfer.DebtorAccountIdentifier, -transfer.Amount);
+        AddTransaction(transfer.CreditorAccountIdentifier, transfer.Amount);
+    }
 
-	public void Withdraw(string accountIdentifier, float amount) => AddTransaction(accountIdentifier, -1 * amount);
+    public void Withdraw(string accountIdentifier, decimal amount) => AddTransaction(accountIdentifier, -1 * amount);
 
-	private void AddTransaction(string accountIdentifier, float amount)
-	{
-		MyBalance accountBalance = _accounts[accountIdentifier];
-		_accounts[accountIdentifier] =
-			accountBalance with { Balance = accountBalance.Balance + amount };
-	}
+    private void AddTransaction(string accountIdentifier, decimal amount)
+    {
+        var accountBalance = _accounts[accountIdentifier];
+
+        var newBalance = accountBalance.Balance + amount;
+
+        _accounts[accountIdentifier] = new MyBalance(newBalance);
+    }
 }
 
-public record MyBalance(float Balance = 0);
+public record MyBalance(decimal Balance = 0);
